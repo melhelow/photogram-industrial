ebe31e0a45485b1a8d6f4fcdf5f804553247cba8
Elhelw, Mohamed <114217025+melhelow@users.noreply.github.com>
2025-07-27 09:40:19 +0000

    crrated extra files and matched the target ui

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -2,6 +2,7 @@ require "active_support/core_ext/integer/time"
 
 Rails.application.configure do
   # Allow server to be hosted on any URL
+   config.hosts << /.*\.github\.dev/
   config.hosts.clear
   # Allow better_errors to work in online IDE
   config.web_console.allowed_ips = "0.0.0.0/0.0.0.0"
--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,18 @@
-# config/routes.rb
 Rails.application.routes.draw do
-  root "users#feed"
-
+  root "photos#index"
   devise_for :users
-
+  
   resources :likes
   resources :follow_requests
   resources :comments
   resources :photos
-  resources :users, only: [:index]
-
-  # These custom routes must be LAST
-  get ":username" => "users#show", as: :user
+  
+  # Custom user routes - must come AFTER resources :photos
+  get ":username" => "users#show", as: :user, constraints: { username: /[a-zA-Z0-9_]+/ }
   get ":username/liked" => "users#liked", as: :liked
   get ":username/feed" => "users#feed", as: :feed
   get ":username/discover" => "users#discover", as: :discover
+  
+  # Search route
+  get "search" => "users#search", as: :search_users
 end
