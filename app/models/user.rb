6406e554013ee7a3d406f1b8c78c35e5f2ad7ccd
Elhelw, Mohamed <114217025+melhelow@users.noreply.github.com>
2025-07-27 00:35:39 +0000

    database

--- app/models/user.rb
+++ app/models/user.rb
@@ -37,26 +37,29 @@ class User < ApplicationRecord
   has_many :comments , foreign_key: :author_id 
   
   has_many :sent_follow_requests, foreign_key: :sender_id, class_name: "FollowRequest"
+  has_many :following, through: :sent_follow_requests, source: :recipient
   has_many :accepted_follow_requests, -> { accepted }, foreign_key: :sender_id, class_name: "FollowRequest"
 
   has_many :received_follow_requests, foreign_key: :recipient_id, class_name: "FollowRequest"
+  has_many :followers, through: :received_follow_requests, source: :sender
  
   has_many :accepted_received_follow_requests, -> { accepted }, foreign_key: :recipient_id, class_name: "FollowRequest"
 
   has_many :likes, foreign_key: :fan_id
   
   has_many :own_photos, foreign_key: :owner_id, class_name: "Photo"
+  has_many :own_photos, class_name: "Photo", foreign_key: "owner_id", dependent: :destroy
   has_many :liked_photos, through: :likes, source: :photo
   has_many :leaders, through: :accepted_sent_follow_requests, source: :recipient
   has_many :leaders, through: :accepted_received_follow_requests, source: :sender
 
-has_many :feed, through: :leaders, source: :own_photos
+  has_many :feed, through: :leaders, source: :own_photos
 
-has_many :discover, through: :leaders, source: :liked_photos
+  has_many :discover, through: :leaders, source: :liked_photos
 
-validates :username, presence: true, uniqueness: true
+  validates :username, presence: true, uniqueness: true
 
-scope :past_week, -> { where(created_at: 1.week.ago...) }
-scope :by_likes, -> { order(likes_count: :desc) }
+  scope :past_week, -> { where(created_at: 1.week.ago...) }
+  scope :by_likes, -> { order(likes_count: :desc) }
 
 end
